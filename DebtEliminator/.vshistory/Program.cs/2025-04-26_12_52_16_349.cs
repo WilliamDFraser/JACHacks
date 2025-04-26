eed033ef09489c5d2a6b480757ee1c3defe6256d
WilliamDF <6286802@johnabbottcollege.net>
2025-04-26 13:37:18 -0400

    Methods written

---
+++ DebtEliminator/.vshistory/Program.cs/2025-04-26_12_52_16_349.cs
@@ -0,0 +1,26 @@
+﻿namespace DebtEliminator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DebtEliminator");
+            Snowball();
+        }
+        static void Snowball()
+        {
+            char input = 'D';
+            bool 
+            Console.WriteLine("Would you like to learn about the Snowball Method? (y/n)");
+    
+        }
+        static void Avalanche()
+        {
+            Console.WriteLine("Would you like to learn about Avalanche Method");
+        }
+        static void Consolidation()
+        {
+            Console.WriteLine("Would you like to learn about debt consolidation");
+        }
+    }
+}
