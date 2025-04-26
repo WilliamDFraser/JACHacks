a95ffa30d7d86cbef29c258dc84ccbcfc7e8627f
islam <islamsaedtsay@gmail.com>
2025-04-26 13:32:13 -0400

    Finished the Debt Class

--- DebtEliminator/obj/Debug/net8.0/DebtEliminator.AssemblyInfo.cs
+++ DebtEliminator/obj/Debug/net8.0/DebtEliminator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DebtEliminator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78d7fb5d4767e3390e292c658e687c920e080303")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40703a29813d53ee12b4638732d31e02b17756ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("DebtEliminator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DebtEliminator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
